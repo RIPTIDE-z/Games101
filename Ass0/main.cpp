6ec5385ae64336f216416538e7e13db9962918bb
zxr <1523871672@qq.com>
2025-03-30 11:53:58 +0800

    Completed basic requirements for ASS0 :simple examples of eigen

--- Ass0/main.cpp
+++ Ass0/main.cpp
@@ -4,47 +4,57 @@
 #include<iostream>
 
 int main(){
-
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
-
     // Example of vector
-    std::cout << "Example of vector \n";
+    std::cout << "1. Example of vector \n";
     // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    Eigen::Vector3i v1(1, 2, 3);
+    Eigen::Vector3f v2(1.f,2.f,3.f);
+    Eigen::Vector3f v3(1.0f,0.0f,0.0f);
     // vector output
     std::cout << "Example of output \n";
-    std::cout << v << std::endl;
+    std::cout << "v1 = \n" << v1 << std::endl
+              << "v2 = \n" << v2 << std::endl
+              << "v3 = \n" << v3 << std::endl;
+
+    std::cout << std::endl;
+
     // vector add
     std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
+    std::cout << "v2 + v3 = \n" << v2 + v3 << std::endl;
+
+    std::cout << std::endl;
+
     // vector scalar multiply
     std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
+    std::cout << "v1 * 3 = \n" << v1 * 3 << std::endl;
+    std::cout << "v2 * 2 = \n" << 2.0f * v2 << std::endl;
+
+    std::cout << std::endl;
 
     // Example of matrix
-    std::cout << "Example of matrix \n";
+    std::cout << "2. Example of matrix \n\n";
     // matrix definition
     Eigen::Matrix3f i,j;
     i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
     j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
     // matrix output
-    std::cout << "Example of output \n";
+    std::cout << "Example of output \ni = \n";
     std::cout << i << std::endl;
+    std::cout << "j =\n" << j << std::endl;
     // matrix add i + j
+    std::cout << "\nExample of add \n";
+    std::cout << "i + j =\n" << i + j << std::endl;
     // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
+    std::cout << "\nExample of scalar multiply \n";
+    std::cout << "i * 2.0 =\n" << i * 2.0 << std::endl;
 
-    std::cin.get();
+    // matrix multiply i * j
+    std::cout << "\nExample of multiply \n";
+    std::cout << "i * j =\n" << i * j << std::endl;
+
+    // matrix multiply vector i * v
+    std::cout << "\nExample of multiply vector\n";
+    std::cout << "i * v2 =\n" << i * v2 << std::endl;
 
     return 0;
 }
